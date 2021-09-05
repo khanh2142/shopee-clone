349e79d494bfec0b3b6377ce63fe6a7a8d769206
unknown <k2142a@gmail.com>
2021-09-05 11:14:32 +0700

    up

--- header.js
+++ header.js
@@ -4,7 +4,17 @@ const $$ = document.querySelectorAll.bind(document);
 const menuList = $('.menu-list');
 const userMenu = $('.user-menu');
 const closeMenu = $('.close-menu');
-const openSignin = $('.wrapper-user-signin');
+const openLoginMobile = $('#login-btn');
+const openSigninMobile = $('#signin-btn');
+
+openLoginMobile.addEventListener('click', () => {
+    window.location = 'login.html';
+});
+
+openSigninMobile.addEventListener('click', () => {
+    window.location = 'login.html';
+});
+
 
 menuList.addEventListener('click', () => {
     userMenu.classList.toggle('active');
