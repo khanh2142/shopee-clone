ef7f16e26be6e10f698ca01f51bb003e7019a41b
unknown <k2142a@gmail.com>
2021-09-06 20:26:02 +0700

    fix carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,19 +1,90 @@
 var slideIndex = 0;
-showSlides();
+const slides = document.querySelectorAll('.mySlides');
+const dots = document.querySelectorAll('.dot');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
+
+
+next.addEventListener('click',() => {
+
+  if (slideIndex == slides.length-1) slideIndex = 0;else slideIndex += 1;
+
+  slides.forEach(slide => {
+    slide.classList.remove('active');
+  });
+
+  slides[slideIndex].classList.add('active');
+
+  dots.forEach(dot => {
+    dot.classList.remove('active');
+  });
+
+  dots[slideIndex].classList.add('active');
+
+});
+
+
+prev.addEventListener('click',() => {
+
+  console.log(slideIndex);
+
+  (slideIndex == 0) ? slideIndex = slides.length-1 : --slideIndex;
+
+  slides.forEach(slide => {
+    slide.classList.remove('active');
+  });
+
+  slides[slideIndex].classList.add('active');
+
+  dots.forEach(dot => {
+    dot.classList.remove('active');
+  });
+
+  dots[slideIndex].classList.add('active');
+
+});
+
+dots.forEach((dot,index )=> {
+  dot.addEventListener('click',() => {
+    slideIndex = index;
+
+    slides.forEach(slide => {
+      slide.classList.remove('active');
+    });
+  
+    slides[slideIndex].classList.add('active');
+  
+    dots.forEach(dot => {
+      dot.classList.remove('active');
+    });
+  
+    dots[slideIndex].classList.add('active');
+  });
+});
+
+
+function slideShow() {
+
+  slides.forEach(slide => {
+    slide.classList.remove('active');
+  });
+
+  slides[slideIndex].classList.add('active');
+
+  dots.forEach(dot => {
+    dot.classList.remove('active');
+  });
+
+  dots[slideIndex].classList.add('active');
+
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+  if (slideIndex >= slides.length) slideIndex = 0; 
+
+  setTimeout(slideShow,2000);
+  
+};
+
+slideShow();
+
+
