c5f34983a65599d0f1518cb1b5eccd24ac9ef985
unknown <k2142a@gmail.com>
2021-09-11 14:28:50 +0700

    fix suggest

--- js/suggest.js
+++ js/suggest.js
@@ -9,3 +9,10 @@ suggestTitle.forEach((item,index) => {
         suggestBar[index].classList.add('active');
     });
 });
+
+const suggestPromotion = [
+    "https://cf.shopee.vn/file/d23804e5a1d134511626ca364f439e65", // Freeship Xtra
+    "https://cf.shopee.vn/file/94d8e46e212bf2ab327c1255f0c8ba64", // Hoàn xu Xtra
+    "https://cf.shopee.vn/file/1060ad975ab9c09fe684673974c1fca3", // Hàng hiệu -50%
+    "https://cf.shopee.vn/file/2eca1ef73b3396fa477abc8f018a11d7" // Freeship Xtra + Hoàn xu Xtra
+];
